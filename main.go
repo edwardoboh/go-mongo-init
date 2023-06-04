d0be522bc07183693d52df1b633b3a033d259bc2
tingo <macbook@Macbooks-MBP.lan>
2023-06-04 15:19:19 +0100

    feat: create collections and records

--- main.go
+++ main.go
@@ -20,25 +20,58 @@ func loadEnv() {
 func main() {
 	loadEnv()
 
+	// establish connection to the server
 	client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGO_URI")))
 	if err != nil {
 		log.Fatal("An error occured with database connection")
 		os.Exit(1)
 	}
 
+	// create context to be used by the connection
 	ctx, _ := context.WithTimeout(context.Background(), time.Second*30)
 
+	// make actual connection to the server, while also checking for errors
 	err = client.Connect(ctx)
 	if err != nil {
 		log.Fatal("Connection to db has failed")
 	}
 
+	// close the connection when this function exists
 	defer client.Disconnect(ctx)
 
-	names, err := client.ListDatabaseNames(ctx, bson.M{})
+	// create database and collections
+	quickstart := client.Database("quickstart")
+	podcast := quickstart.Collection("podcast")
+	episode := quickstart.Collection("episode")
+
+	// create a new single document
+	createdPodcast, err := podcast.InsertOne(ctx, bson.D{
+		{Key: "title", Value: "The Joe Rogan Podcast"},
+		{"author", "Joe Rogan"},
+		{"medium", "Spotify"},
+	})
+
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error while creating Podcast")
 	}
 
-	fmt.Print(names)
+	// insert multiple documents at a go
+	createdEpisodes, err := episode.InsertMany(ctx, []interface{}{
+		bson.D{
+			{Key: "podcast", Value: createdPodcast.InsertedID},
+			{Key: "title", Value: "Episode #1 - We Begin"},
+			{Key: "description", Value: "This is the first episode in this podcast series"},
+			{Key: "duration", Value: 25},
+		},
+		bson.D{
+			{"podcast", createdPodcast.InsertedID},
+			{"title", "The New Arena"},
+			{"description", "This will help folks enter new arenas"},
+			{"duration", 37},
+			{"tags", bson.A{"though", "experiment", "era", "change"}},
+		},
+	})
+
+	fmt.Println("Podcast ID: ", createdPodcast.InsertedID)
+	fmt.Println("Episodes ID: ", createdEpisodes.InsertedIDs)
 }
